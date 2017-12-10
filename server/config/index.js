2b2cec2baca8cbc9669eee622a1c48f7263ad6b9
Danil <danil.kazim.99@gmail.com>
2017-12-10 20:22:41 +0200

    webprogbase v2

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,3 @@
-require('module-alias/register');
-
-
 module.exports = {
-    secret: process.env.SECRET || "raisawesome",
-    session: {session: false},
-    // database: 'mongodb://127.0.0.1:27017/application',
-    database: `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@ds042527.mlab.com:42527/codual_db`
-}
+    PORT: process.env.PORT || 3000,
+};
--- server/database/models/models.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose'),
-    User = require('@CodualDBModels/DB.models.User'),
-    Publication = require('@CodualDBModels/DB.models.Publication')
-module.exports = {
-    User: mongoose.model('User'),
-    Publication: mongoose.model('Publication')
-}
