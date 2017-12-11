8fdd44828b8ee9ca6205674dd93ef05e4dc49cf5
Danil <danil.kazim.99@gmail.com>
2017-12-11 11:01:28 +0200

    fix: +facebook callback

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     ROOT_URL: (process.env.NODE_ENV == 'dev') ?
-        `localhost:${this.PORT}` : '',
+        `localhost:${this.PORT}` : 'https://codual.herokuapp.com',
     DB: (process.env.NODE_ENV == 'dev') ?
         'mongodb://localhost:27017/webprogbase2' :
         `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds042527.mlab.com:42527/codual_db`,
@@ -9,7 +9,7 @@ module.exports = {
         facebook: {
             APP_ID: process.env.FACEBOOK_APP_ID,
             APP_SECRET: process.env.FACEBOOK_APP_SECRET,
-            CALLBACK_URL: this.ROOT_URL + '/api/v1/facebook/token'
+            CALLBACK_URL: this.ROOT_URL + '/api/v1/auth/facebook/token'
         }
     },
     security: {
