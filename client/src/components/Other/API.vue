85fc97b102d80993b5fbefee4a45a31ee0dc4ad1
Danil <danil.kazim.99@gmail.com>
2017-12-13 00:45:07 +0200

    add: +vue page api

--- client/src/components/Other/API.vue
+++ client/src/components/Other/API.vue
@@ -1,18 +1,49 @@
 <template>
-
+  <div class="container">
+    <div class="column is-8-desktop  is-offset-2-desktop  is-12-touch">
+      <div class="box">
+        <div class="content markdown-body" v-html="text"/>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+  import API from '#/API';
+
   export default {
     data () {
-      return {}
+      return {
+        text: null
+      }
     },
-    methods: {},
+    methods: {
+      error (msg) {
+        this.$toast.open({
+          duration: 5000,
+          message: msg || "Something is going wrong",
+          position: 'is-top',
+          type: 'is-danger'
+        })
+      },
+      async loadText () {
+        try {
+          const result = await API.noAuth().get('/api/v1/docs');
+          if (result.data.success) {
+            this.text = result.data.item;
+          }
+        } catch (err) {
+          this.error(err.message);
+        }
+      }
+    },
     computed: {},
     props: [],
-    created () {
-
-    }
+    async created () {
+      await this.loadText();
+    },
   }
 </script>
 <style scoped lang="scss">
+  @import '~github-markdown-css';
+
 </style>
