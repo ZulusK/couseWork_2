ddf3ffd09806998326ddacf7e8be6e6d945d884e
Danil <danil.kazim.99@gmail.com>
2017-12-21 12:56:16 +0200

    prod build

--- client/src/services/globals.js
+++ client/src/services/globals.js
@@ -1,7 +1,7 @@
 export default {
   ROOT_URL: process.env.NODE_ENV.startsWith('dev')
     ? `http://localhost:${process.env.PORT || 3000}`
-    : `https://codual.herokuapp.com`,
+    : ``,
   PLACEHOLDER: '/static/img/placeholder.png',
   PAGINATION_LIMIT: 10
 }
